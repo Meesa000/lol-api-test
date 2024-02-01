c11fd0dca97c9268746e62ccd7762de4a970b2aa
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-02-01 12:49:39 +0000

    add api call to get summoner name

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const submitButton = document.getElementById('submit').addEventListener('click',searchForPlayer);
+const displaySummonerName = document.getElementById('summoner-name');
+
+
+
+
+
+
+function searchForPlayer(){
+    const API_KEY = 'RGAPI-2c53371a-b1e0-40c8-a34b-41cecf955f37'
+    const summonerName = document.getElementById('search-player').value;
+    const apiURL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+summonerName+'?api_key='+API_KEY
+
+    fetch(apiURL)
+    .then(handleResponse)
+    .then(updateSummonerName)
+    .catch(handleError);
+}
+
+function handleResponse(response) {
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json(); // Call json() here to parse the response body as JSON
+}
+
+function updateSummonerName(data) {
+    // Access the 'name' property from the parsed JSON data
+    const summonerNameResult = data.name;
+
+    // Update the <h3> element with the summoner name
+    const h3Element = document.getElementById('summoner-name');
+    h3Element.textContent = `Summoner Name: ${summonerNameResult}`;
+}
+
+function handleError(error) {
+    // Handle errors
+    console.error('Error:', error);
+}
